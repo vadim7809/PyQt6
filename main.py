d21a4ebe27bbf1b3e12795480240e08fb60b9ea0
vadim7809 <mandyuk.vadim@gmail.com>
2025-08-06 12:00:44 +0300

    py

--- main.py
+++ main.py
@@ -1,70 +1,82 @@
 from PyQt6.QtWidgets import *
+from PyQt6.QtCore import QTimer
+from PyQt6.QtGui import QIcon
 import requests
 
 
-def get_price():
+def convert():
     crypto_id = crypto_box.currentText()
     currency = currency_box.currentText()
+    direction = direction_box.currentText()
+    amount_text = amount_input.text()
 
     try:
         url = "https://api.coingecko.com/api/v3/simple/price"
-        params = {
-            "ids": crypto_id,
-            "vs_currencies": currency
-        }
+        params = {"ids": crypto_id,
+                  "vs_currencies": currency}
+
         response = requests.get(url, params=params)
         data = response.json()
-
         price = data[crypto_id][currency]
-        price_lbl.setText(f"{crypto_id.capitalize()} = {price} {currency.upper()}")
-    except Exception as e:
-        price_lbl.setText(f"Помилка: {e}")
 
-    amount_text = h.text()
-    if amount_text:
-        try:
-            amount = float(amount_text)
-            total = amount * price, 2
-            price_lbl.setText(f"{crypto_id.capitalize()} = {price} {currency.upper()}")
-        except ValueError:
-            price_lbl.setText("Невірне число")
+        amount = float(amount_text)
+
+        if direction == "Крипта > Валюта":
+            result = round(amount * price, 2)
+            result_lbl.setText(f"{amount} {crypto_id} = {result} {currency.upper()}")
+        else:
+            result = round(amount / price, 8)
+            result_lbl.setText(f"{amount} {currency.upper()} = {result} {crypto_id}")
+    except:
+        result_lbl.setText("Помилка або некоректне число")
+        print(result_lbl)
+
+
+
+timer = QTimer()
+timer.setInterval(10000)
+timer.timeout.connect(convert)
+timer.start()
+
 app = QApplication([])
 
 window = QWidget()
-window.setWindowTitle("Конвертер криптовалют")
 window.resize(600, 400)
 
-main_line = QVBoxLayout()
+layout = QVBoxLayout()
 
-crypto_lbl = QLabel("Криптовалюта")
 crypto_box = QComboBox()
-crypto_box.addItems(["bitcoin", "ethereum", "solana", "dogecoin", "cardano"])
+crypto_box.addItem(QIcon("btc.png"), "BTC")
+crypto_box.addItem(QIcon("sol.png"), "SOL")
+crypto_box.addItem(QIcon("eth.png"), "ETH")
 
 
-currenc_lbl = QLabel("Фіатна валюта")
 currency_box = QComboBox()
+currency_box.addItem(QIcon("usd.png"), "USD")
+currency_box.addItem(QIcon("eur.png"), "EUR")
+currency_box.addItem(QIcon("uah.png"), "UAH")
 
-currency_box.addItems(["usd", "eur", "uah", "gbp"])
+direction_box = QComboBox()
+direction_box.addItems(["Крипта > Валюта", "Валюта > Крипта"])
 
-
-h = QLabel("Введіть суму криптовалюти яка у вас є")
-h1 = QLineEdit()
-#h2 = QLabel("Наприклад 0.5 ")
-
-price_lbl = QLabel("Оберіть криптовалюту та валюту")
-price_btn = QPushButton("Оновити ціну")
-price_btn.clicked.connect(get_price)
-
-main_line.addWidget(h)
-main_line.addWidget(h1)
-main_line.addWidget(crypto_lbl)
-main_line.addWidget(crypto_box)
-main_line.addWidget(currenc_lbl)
-main_line.addWidget(currency_box)
-main_line.addWidget(price_btn)
-main_line.addWidget(price_lbl)
+amount_input = QLineEdit()
 
 
-window.setLayout(main_line)
+convert_btn = QPushButton("Конвертувати")
+convert_btn.clicked.connect(convert)
+
+result_lbl = QLabel("Результат:")
+
+layout.addWidget(QLabel("Криптовалюта:"))
+layout.addWidget(crypto_box)
+layout.addWidget(QLabel("Валюта:"))
+layout.addWidget(currency_box)
+layout.addWidget(QLabel("Напрямок:"))
+layout.addWidget(direction_box)
+layout.addWidget(amount_input)
+layout.addWidget(convert_btn)
+layout.addWidget(result_lbl)
+
+window.setLayout(layout)
 window.show()
 app.exec()
